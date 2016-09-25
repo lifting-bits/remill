8b3dc199dbd74535d8a2a4a7707cd9362b17a109
Peter Goodman <peter.goodman@gmail.com>
2016-09-25 18:12:28 -0400

    Adding more test generation and compilation. Unfortunately compiling tests isn't working due to llvm::CloneFunctionInto not quite working anymore.

--- scripts/x86/generate_tests.sh
+++ scripts/x86/generate_tests.sh
@@ -5,7 +5,7 @@ DIR=$(dirname $(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )))
 
 function generate_tests()
 {
-    pushd /tmp
+    printf "Generating tests for ${4}\n"
     clang++-3.9 \
         -I${DIR} \
         -std=gnu++11 \
@@ -30,12 +30,58 @@ function generate_tests()
     
     ./a.out > $DIR/generated/Arch/X86/Tests/${4}.cfg
     rm ./a.out
-    popd
+}
+
+function lift_tests()
+{
+    printf "Lifting tests for ${1}\n"
+    remill-lift --cfg $DIR/generated/Arch/X86/Tests/${1}.cfg \
+                --arch_in ${1} --arch_out amd64 \
+                --bc_out $DIR/generated/Arch/X86/Tests/${1}.cfg.bc
+}
+
+function compile_tests()
+{
+    printf "Compiling tests for ${4}\n"
+    clang++-3.9 \
+        -I${DIR} \
+        -std=gnu++11 \
+        -Wno-nested-anon-types \
+        -Wno-variadic-macros \
+        -Wno-extended-offsetof \
+        -Wno-return-type-c-linkage \
+        -Wno-expansion-to-defined \
+        -m64 \
+        -I${DIR} \
+        -DADDRESS_SIZE_BITS=${1} \
+        -DHAS_FEATURE_AVX=${2} \
+        -DHAS_FEATURE_AVX512=${3} \
+        $DIR/tests/X86/Tests.S \
+        $DIR/tests/X86/Run.cpp \
+        $DIR/generated/Arch/X86/Tests/${1}.cfg.bc \
+        -lgflags \
+        -lglog 
+    
+    ./a.out > $DIR/generated/Arch/X86/Tests/${4}
+    rm ./a.out
 }
 
 mkdir -p $DIR/generated/Arch/X86/Tests
+
+pushd /tmp
 
 generate_tests 32 0 0 x86
 generate_tests 32 1 0 x86_avx
 generate_tests 64 0 0 amd64
 generate_tests 64 1 0 amd64_avx
+
+lift_tests x86
+lift_tests x86_avx
+lift_tests amd64
+lift_tests amd64_avx
+
+compile_tests 32 0 0 x86
+compile_tests 32 1 0 x86_avx
+compile_tests 64 0 0 amd64
+compile_tests 64 1 0 amd64_avx
+popd
