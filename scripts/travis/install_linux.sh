657e4b580d65234cba3ee473ca8358057a847444
Peter Goodman <peter.goodman@gmail.com>
2016-10-01 17:39:18 -0400

    Re-order the linux-specific travis install script. Hope that protocol buffers gets installed correctly this time. Remove test generation and running from end of install script -- it doesn't belong there.

--- scripts/travis/install_linux.sh
+++ scripts/travis/install_linux.sh
@@ -4,36 +4,35 @@
 DIR=$(dirname $(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )))
 UBUNTU_RELEASE=`lsb_release -sc`
 
+# Make sure we have `add-apt-repository`.
 sudo apt-get update -qq
-#sudo apt-get upgrade -yqq
+sudo apt-get install -yqq software-properties-common
 
-sudo apt-get install -yqq \
+# Add the LLVM repositories and keys.
+wget -qO - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo add-apt-repository -y "deb http://apt.llvm.org/${UBUNTU_RELEASE}/ llvm-toolchain-${UBUNTU_RELEASE} main"
+sudo add-apt-repository -y "deb http://apt.llvm.org/${UBUNTU_RELEASE}/ llvm-toolchain-${UBUNTU_RELEASE}-3.8 main"
+sudo add-apt-repository -y "deb http://apt.llvm.org/${UBUNTU_RELEASE}/ llvm-toolchain-${UBUNTU_RELEASE}-3.9 main"
+
+# Add the CMake repository.
+sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+
+# Update sources list, and then install needed packages.
+sudo apt-get update -qq
+sudo apt-get install -yq \
      git \
      libgoogle-glog-dev \
      libgtest-dev \
      libprotoc-dev libprotobuf-dev libprotobuf-dev protobuf-compiler \
      python2.7 python-pip \
+     llvm-3.9-dev clang-3.9 \
      libc++-dev libc++-dev:i386 \
      libc6-dev libc6-dev:i386 \
      unzip \
-     software-properties-common
+     cmake
 
-wget -qO - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-
-# For LLVM.
-sudo add-apt-repository -y "deb http://apt.llvm.org/${UBUNTU_RELEASE}/ llvm-toolchain-${UBUNTU_RELEASE} main"
-sudo add-apt-repository -y "deb http://apt.llvm.org/${UBUNTU_RELEASE}/ llvm-toolchain-${UBUNTU_RELEASE}-3.8 main"
-sudo add-apt-repository -y "deb http://apt.llvm.org/${UBUNTU_RELEASE}/ llvm-toolchain-${UBUNTU_RELEASE}-3.9 main"
-
-# For CMake
-sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
-
-sudo apt-get update -qq
-#sudo apt-get upgrade -yqq
-sudo apt-get install -yqq llvm-3.9-dev clang-3.9 cmake
-
+# Upgrade PIP and install the python bindings for protocol buffers.
 sudo pip install --upgrade pip
-
 sudo pip install python-magic 'protobuf==2.4.1'
 
 # Unpack and install Intel XED.
@@ -59,5 +58,3 @@ make all
 sudo make install
 popd
 
-$DIR/scripts/x86/generate_tests.sh
-$DIR/scripts/x86/run_tests.sh
