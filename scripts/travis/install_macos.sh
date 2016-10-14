8546f18f8b58f6688f707bf081153e4589d3ba5e
JP Smith <jp.smith@ilstu.edu>
2016-10-13 21:49:03 -0500

    fix unnecessary brew stuff for os x

--- scripts/travis/install_macos.sh
+++ scripts/travis/install_macos.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 # Copyright 2016 Peter Goodman (peter@trailofbits.com), all rights reserved.
 
-brew install git
-brew install cmake
 brew install glog
 brew install protobuf
-brew install python
-brew install coreutils
-brew install unzip
 
 $DIR/build.sh
