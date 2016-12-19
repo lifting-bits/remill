29b49152ffa05f9ed340a0b1b4273944eb0e110c
andrewreiter <arr@watson.org>
2016-12-19 10:52:15 -0500

    Add Module generation and output. There is some byte muckery that I adjusted because I created my own script with python3 and am porting this down to 2. There I use bytes.fromhex() and here I use some hand manipulation what-have-u: which I hope is correct. Needs testing.... Needs some comparison with what would be expected from IDA for some given program... Avoids the program API which is likely not ideal ?

--- tools/disass/radare2/disass.py
+++ tools/disass/radare2/disass.py
@@ -1,8 +1,18 @@
-
+import CFG_pb2
 try:
     import r2pipe
 except:
     print("Install the radare2 python bindings. (https://github.com/radare/radare2-r2pipe)")
+
+#
+# It seems the proper way is to use 'program' API, but I was feeling
+# confused on the purpose and source of some of the information to provide the calls.
+# For example, is the Instruction personality field something I should be
+# concerned with? It does not get translated to the generated protobuf stream,
+# so my opinion is no. But, I do not fully get the IDA interface, so it
+# is possible I am being very naive. 
+# 
+# So, first whack this way and then go from there... c'est la vie.
 
 
 def execute(args, command_args):
@@ -40,8 +50,18 @@ def execute(args, command_args):
     #   "nlocals":0
     # }
     fn_list = r2.cmdj("aflj")
+
+    mod = CFG_pb2.Module()
     for fne in fn_list:
         name = fne["name"]
+        new_block = True
+
+        # Having each function be a named block. Is enough?
+        named_block = CFG_pb2.NamedBlock()
+        named_block.name = name
+        named_block.address = fne["offset"]
+        named_block.visibility = 1	# XXX: Ehm; must revisit.
+        mod.named_blocks.extend([named_block])
 
         # Analyze the current function
         r2.cmd("af@{0}".format(name))
@@ -71,5 +91,46 @@ def execute(args, command_args):
         #      }, ... ]
         # }
         dis = r2.cmdj("pdfj@{0}".format(name))
-   
+        for op in dis["ops"]:
+            if new_block == True:
+                print "new Block"
+                if "bytes" not in op.keys():
+                    print "Skipp"
+                    continue
+                blk = CFG_pb2.Block()
+                blk.address = op["offset"]
+                new_block = False
+            else:
+                print "old Block"
+           
+            if "bytes" not in op.keys():
+                if new_block == False:
+                    mod.blocks.extend([blk])
+                    new_block = True
+                    blk = None
+                continue
+
+            ii = CFG_pb2.Instr()
+
+            # I was using python3 before so had bytes.fromhex() API call,
+            # but I believe this is the correct juju for Python2
+            btmp = [op["bytes"][i:i+2] for i in range(0, len(op["bytes"]), 2)]
+            btmp = [int(x, 16) for x in btmp]
+            ii.bytes = ''.join(chr(x) for x in btmp)
+            ii.address = op["offset"]    
+            blk.instructions.extend([ii])
+           
+            # XXX: Unsure if want new block after a jump.
+            # also, my jump check may be "too loose". 
+            if op["opcode"].startswith("call") == True or \
+               op["opcode"].startswith("ret") == True or \
+               op["opcode"].startswith("j") == True:
+                mod.blocks.extend([blk])
+                new_block = True
+                blk = None
+                continue
+
+    ostream = open(args.output, "wb")
+    ostream.write(mod.SerializeToString())
+    ostream.close()
     return 0
