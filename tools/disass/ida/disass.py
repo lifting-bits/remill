58d71818fbe4e914f293192cb79217636ca382aa
Douglas Goddard <douglas.gastonguay@gmail.com>
2017-01-30 12:53:02 -0500

    Removed unnecessary import.

--- tools/disass/ida/disass.py
+++ tools/disass/ida/disass.py
@@ -8,7 +8,6 @@ import os
 import subprocess
 import sys
 import traceback
-from bisect import bisect_right
 
 # Import modules from parent directory
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
