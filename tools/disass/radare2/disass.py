f471de7913103c921e9227195e0a8eb66fb11b4e
Andrew R. Reiter <arr@watson.org>
2017-02-23 08:56:39 -0800

    Use log module. Some minimal cleanup and some comments adjusted and removed. There is likely some work to be done but possibly this is the last commit before pull req

--- tools/disass/radare2/disass.py
+++ tools/disass/radare2/disass.py
@@ -1,26 +1,22 @@
+
+import log
+
 import CFG_pb2
 import x86
+
 try:
     import r2pipe
 except:
     print("Install the radare2 python bindings. (https://github.com/radare/radare2-r2pipe)")
 
-#
-# It seems the proper way is to use 'program' API, but I was feeling
-# confused on the purpose and source of some of the information to provide the calls.
-# For example, is the Instruction personality field something I should be
-# concerned with? It does not get translated to the generated protobuf stream,
-# so my opinion is no. But, I do not fully get the IDA interface, so it
-# is possible I am being very naive. 
-# 
-# So, first whack this way and then go from there... c'est la vie.
-
 
 def execute(args, command_args):
+    log.init(output_file=args.log_file, log_level=args.log_level)
+
 
     target_arch = args.arch
     if target_arch != "x86":
-        print("Only x86 is currently supported.")
+        log.critical("Only x86 is currently supported.")
         return 1
 
     # This is really not used. r2pipe expects that
@@ -30,7 +26,6 @@ def execute(args, command_args):
     mod = None
 
     r2 = r2pipe.open(args.binary, flags=["-a", target_arch])
-    r2.cmd("aa") # ``Analyze all''
     if target_arch == "x86":
         mod = x86.analyze(r2)
 
