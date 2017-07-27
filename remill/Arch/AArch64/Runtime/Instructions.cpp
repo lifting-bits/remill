b011bc47b41a0768dcc9c158b8a2662b63772528
Peter Goodman <peter.goodman@gmail.com>
2017-07-27 19:21:11 -0400

    Fixup some macros

--- remill/Arch/AArch64/Runtime/Instructions.cpp
+++ remill/Arch/AArch64/Runtime/Instructions.cpp
@@ -28,48 +28,48 @@
 
 #define REG_PC state.gpr.PC.qword
 #define REG_SP state.gpr.SP.qword
-#define REG_LP state.gpr.X30.qword
-#define REG_FP state.gpr.X29.qword
-#define REG_XZR state.gpr.X31.qword
+#define REG_LP state.gpr.x30.qword
+#define REG_FP state.gpr.x29.qword
+#define REG_XZR state.gpr.x31.qword
 
-#define REG_X0 state.gpr.X0.qword
-#define REG_X1 state.gpr.X1.qword
-#define REG_X2 state.gpr.X2.qword
-#define REG_X3 state.gpr.X3.qword
+#define REG_X0 state.gpr.x0.qword
+#define REG_X1 state.gpr.x1.qword
+#define REG_X2 state.gpr.x2.qword
+#define REG_X3 state.gpr.x3.qword
 
-#define REG_X4 state.gpr.X4.qword
-#define REG_X5 state.gpr.X5.qword
-#define REG_X6 state.gpr.X6.qword
-#define REG_X7 state.gpr.X7.qword
+#define REG_X4 state.gpr.x4.qword
+#define REG_X5 state.gpr.x5.qword
+#define REG_X6 state.gpr.x6.qword
+#define REG_X7 state.gpr.x7.qword
 
-#define REG_X8 state.gpr.X8.qword
-#define REG_X9 state.gpr.X9.qword
-#define REG_X10 state.gpr.X10.qword
-#define REG_X11 state.gpr.X11.qword
+#define REG_X8 state.gpr.x8.qword
+#define REG_X9 state.gpr.x9.qword
+#define REG_X10 state.gpr.x10.qword
+#define REG_X11 state.gpr.x11.qword
 
-#define REG_X12 state.gpr.X12.qword
-#define REG_X13 state.gpr.X13.qword
-#define REG_X14 state.gpr.X14.qword
-#define REG_X15 state.gpr.X15.qword
+#define REG_X12 state.gpr.x12.qword
+#define REG_X13 state.gpr.x13.qword
+#define REG_X14 state.gpr.x14.qword
+#define REG_X15 state.gpr.x15.qword
 
-#define REG_X16 state.gpr.X16.qword
-#define REG_X17 state.gpr.X17.qword
-#define REG_X18 state.gpr.X18.qword
-#define REG_X19 state.gpr.X19.qword
+#define REG_X16 state.gpr.x16.qword
+#define REG_X17 state.gpr.x17.qword
+#define REG_X18 state.gpr.x18.qword
+#define REG_X19 state.gpr.x19.qword
 
-#define REG_X20 state.gpr.X20.qword
-#define REG_X21 state.gpr.X21.qword
-#define REG_X22 state.gpr.X22.qword
-#define REG_X23 state.gpr.X23.qword
+#define REG_X20 state.gpr.x20.qword
+#define REG_X21 state.gpr.x21.qword
+#define REG_X22 state.gpr.x22.qword
+#define REG_X23 state.gpr.x23.qword
 
-#define REG_X24 state.gpr.X24.qword
-#define REG_X25 state.gpr.X25.qword
-#define REG_X26 state.gpr.X26.qword
-#define REG_X27 state.gpr.X27.qword
+#define REG_X24 state.gpr.x24.qword
+#define REG_X25 state.gpr.x25.qword
+#define REG_X26 state.gpr.x26.qword
+#define REG_X27 state.gpr.x27.qword
 
-#define REG_X28 state.gpr.X28.qword
-#define REG_X29 state.gpr.X29.qword
-#define REG_X30 state.gpr.X30.qword
+#define REG_X28 state.gpr.x28.qword
+#define REG_X29 state.gpr.x29.qword
+#define REG_X30 state.gpr.x30.qword
 
 #define FLAG_Z state.sr.z  // Zero flag.
 #define FLAG_C state.sr.c  // Carry flag.
