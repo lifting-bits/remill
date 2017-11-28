55237c1a133a9d89a25d65cca6c573edf10c61ab
Peter Goodman <peter.goodman@gmail.com>
2017-11-28 14:55:13 -0500

    Bringing the aarch64 branch back in line with master.

--- remill/BC/Compat/JITSymbol.h
+++ remill/BC/Compat/JITSymbol.h
@@ -13,21 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef REMILL_BC_COMPAT_RUNTIMEDYLD_H_
-#define REMILL_BC_COMPAT_RUNTIMEDYLD_H_
+#ifndef REMILL_BC_COMPAT_JITSYMBOL_H_
+#define REMILL_BC_COMPAT_JITSYMBOL_H_
 
 #include "remill/BC/Version.h"
 
-#include <llvm/ExecutionEngine/RuntimeDyld.h>
-#include <llvm/ExecutionEngine/RTDyldMemoryManager.h>
-
-#if LLVM_VERSION_NUMBER == LLVM_VERSION(3, 9)
+#if LLVM_VERSION_NUMBER < LLVM_VERSION(4, 0)
+# include <llvm/ExecutionEngine/RuntimeDyld.h>
 namespace llvm {
 using JITSymbol = llvm::RuntimeDyld::SymbolInfo;
-class JITSymbolResolver : public llvm::RuntimeDyld::SymbolResolver {};
+using JITSymbolResolver = llvm::RuntimeDyld::SymbolResolver;
 }  // namespace llvm
 #else
-# error "Unsupported LLVM version."
+# include <llvm/ExecutionEngine/JITSymbol.h>
 #endif
-
-#endif  // REMILL_BC_COMPAT_RUNTIMEDYLD_H_
+#endif  // REMILL_BC_COMPAT_JITSYMBOL_H_
