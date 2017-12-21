47a66e3beadf4be3ce8185617cbbc7d4ad83bd15
Peter Goodman <peter.goodman@gmail.com>
2017-12-20 19:54:22 -0500

    Adds in implementation when the intrinsics are not present. (#203)
    * Adds in implementation when the intrinsics are not present.
    
    * Minor fix.

--- remill/Arch/AArch64/Semantics/BITBYTE.cpp
+++ remill/Arch/AArch64/Semantics/BITBYTE.cpp
@@ -154,10 +154,29 @@ DEF_SEM(REV64, R64W dst, R64 src) {
   return memory;
 }
 
+template <typename T, size_t n>
+ALWAYS_INLINE static T ReverseBits(T v) {
+  T rv = 0;
+  _Pragma("unroll")
+  for (size_t i = 0; i < n; ++i, v >>= 1) {
+    rv = (rv << T(1)) | (v & T(1));
+  }
+  return rv;
+}
+
+#if !__has_builtin(__builtin_bitreverse32)
+# define __builtin_bitreverse32(x) ReverseBits<uint32_t, 32>(x)
+#endif
+
 DEF_SEM(RBIT32, R32W dst, R32 src) {
   WriteZExt(dst, __builtin_bitreverse32(Read(src)));
   return memory;
 }
+
+
+#if !__has_builtin(__builtin_bitreverse64)
+# define __builtin_bitreverse64(x) ReverseBits<uint64_t, 64>(x)
+#endif
 
 DEF_SEM(RBIT64, R64W dst, R64 src) {
   Write(dst, __builtin_bitreverse64(Read(src)));
