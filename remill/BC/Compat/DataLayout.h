af772d2a9754fe2109935235f1fc5df09653e642
Azeezah Muhammad <azeezahm14@gmail.com>
2019-12-11 22:30:30 +0000

    Fix style (variable names and whitespace)

--- remill/BC/Compat/DataLayout.h
+++ remill/BC/Compat/DataLayout.h
@@ -1,6 +1,6 @@
 #include <llvm/IR/DataLayout.h>
 
-inline static uint64_t SizeInBits(llvm::DataLayout& DL, llvm::Type *Ty) {
-	IF_LLVM_LT_1000(return DL.getTypeSizeInBits(Ty);)
-	IF_LLVM_GTE_1000(return DL.getTypeSizeInBits(Ty).getFixedSize();)
+inline static uint64_t SizeInBits(llvm::DataLayout &datalayout, llvm::Type *type) {
+	IF_LLVM_LT_1000(return datalayout.getTypeSizeInBits(type);)
+	IF_LLVM_GTE_1000(return datalayout.getTypeSizeInBits(type).getFixedSize();)
 }
