4131a761c1e4e31b8a53f0db3c21460a003050e6
Peter Goodman <peter@trailofbits.com>
2020-08-18 14:16:37 -0400

    LLVMSupport and LLVMCore are out of order in LLVM_AVAILABLE_LIBS

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -88,15 +88,22 @@ function(GetTargetTree output_variable)
         continue()
       endif()
 
+      # Collect the results
+      unset(new_queue_candidates)
+
       # We can only get LINK_LIBRARIES from normal targets
       if(NOT "${target_type}" STREQUAL "INTERFACE_LIBRARY")
         get_target_property(target_link_libs "${target}" LINK_LIBRARIES)
-      else()
-        set(target_link_libs "target_link_libs-NOTFOUND")
+        if(NOT "${target_link_libs}" STREQUAL "target_link_libs-NOTFOUND")
+          list(APPEND new_queue_candidates ${target_link_libs})
+        endif()
       endif()
 
       # INTERFACE_LINK_LIBRARIES are potentially always present
       get_target_property(target_interface_link_libs "${target}" INTERFACE_LINK_LIBRARIES)
+      if(NOT "${target_interface_link_libs}" STREQUAL "target_interface_link_libs-NOTFOUND")
+        list(APPEND new_queue_candidates ${target_interface_link_libs})
+      endif()
 
       # Try to find the actual file
       if ("${target_type}" STREQUAL "UNKNOWN_LIBRARY" OR
@@ -106,22 +113,9 @@ function(GetTargetTree output_variable)
         get_target_property(target_imported_loc "${target}" IMPORTED_LOCATION)
         if(NOT "${target_imported_loc}" STREQUAL "target_imported_loc-NOTFOUND")
           list(APPEND new_queue_candidates "${target_imported_loc}")
-          set(target_link_libs "target_link_libs-NOTFOUND")
-          set(target_interface_link_libs "target_interface_link_libs-NOTFOUND")
         endif()
       endif()
-
-      # Collect the results
-      unset(new_queue_candidates)
-
-      if(NOT "${target_link_libs}" STREQUAL "target_link_libs-NOTFOUND")
-        list(APPEND new_queue_candidates ${target_link_libs})
-      endif()
-
-      if(NOT "${target_interface_link_libs}" STREQUAL "target_interface_link_libs-NOTFOUND")
-        list(APPEND new_queue_candidates ${target_interface_link_libs})
-      endif()
-
+      
       foreach(queue_candidate ${new_queue_candidates})
         list(FIND visited_dependencies "${queue_candidate}" visited)
         if(visited EQUAL -1)
@@ -138,7 +132,9 @@ function(GetTargetTree output_variable)
     set(queue ${new_queue})
   endwhile()
 
+  list(REVERSE visited_dependencies)
   list(REMOVE_DUPLICATES visited_dependencies)
+  list(REVERSE visited_dependencies)
   set("${output_variable}" ${visited_dependencies} PARENT_SCOPE)
 endfunction()
 
