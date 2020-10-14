e17750bd0b4fec8553fb74d8f5f8f420c0d29d11
sschriner <sonya.schriner@trailofbits.com>
2020-10-14 10:51:51 -0400

    Cleaned up some things, updated the decoding semantics and semantics for the logical instructions

--- remill/Arch/AArch32/Semantics/LOGICAL.cpp
+++ remill/Arch/AArch32/Semantics/LOGICAL.cpp
@@ -35,23 +35,6 @@ DEF_SEM(ORRS, R32W dst, R32 src1, I32 src2, I32 src2_rrx, I8 carry_out) {
   return memory;
 }
 
-DEF_SEM(MOV, R32W dst, R32 _, I32 src, I32 src_rrx){
-  auto value = UOr(Read(src), Read(src_rrx));
-  Write(dst, value);
-  return memory;
-}
-
-DEF_SEM(MOVS, R32W dst, R32 _, I32 src, I32 src_rrx, I8 carry_out) {
-  auto value = UOr(Read(src), Read(src_rrx));
-  Write(dst, value);
-
-  state.sr.n = SignFlag(value);
-  state.sr.z = ZeroFlag(value);
-  state.sr.c = Read(carry_out);
-  // PSTATE.V unchanged
-  return memory;
-}
-
 DEF_SEM(BIC, R32W dst, R32 src1, I32 src2, I32 src2_rrx){
   auto value = UNot(UOr(Read(src2), Read(src2_rrx)));
   auto result = UAnd(Read(src1), value);
@@ -71,31 +54,14 @@ DEF_SEM(BICS, R32W dst, R32 src1, I32 src2, I32 src2_rrx, I8 carry_out) {
   return memory;
 }
 
-DEF_SEM(MVN, R32W dst, R32 _, I32 src, I32 src_rrx){
-  auto value = UNot(UOr(Read(src), Read(src_rrx)));
-  Write(dst, value);
-  return memory;
-}
-
-DEF_SEM(MVNS, R32W dst, R32 _, I32 src, I32 src_rrx, I8 carry_out) {
-  auto value = UNot(UOr(Read(src), Read(src_rrx)));
-  Write(dst, value);
-
-  state.sr.n = SignFlag(value);
-  state.sr.z = ZeroFlag(value);
-  state.sr.c = Read(carry_out);
-  // PSTATE.V unchanged
-  return memory;
-}
-
 } // namespace
 
 DEF_ISEL(ORRrrri) = ORR;
 DEF_ISEL(ORRSrrri) = ORRS;
-DEF_ISEL(MOVrrri) = MOV;
-DEF_ISEL(MOVSrrri) = MOVS;
+DEF_ISEL(MOVrrri) = ORR;
+DEF_ISEL(MOVSrrri) = ORRS;
 DEF_ISEL(BICrrri) = BIC;
 DEF_ISEL(BICSrrri) = BICS;
-DEF_ISEL(MVNrrri) = MVN;
-DEF_ISEL(MVNSrrri) = MVNS;
+DEF_ISEL(MVNrrri) = BIC;
+DEF_ISEL(MVNSrrri) = BICS;
 
