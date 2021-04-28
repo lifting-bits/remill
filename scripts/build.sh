9589a9b52a7bc129738d001c43344ea12bb56682
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-04-28 15:03:59 -0400

    CI: Use v0.1.3 (latest) cxx-common prebuilt libraries

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,7 +27,7 @@ LLVM_VERSION=llvm-11
 OS_VERSION=
 ARCH_VERSION=
 BUILD_FLAGS=
-CXX_COMMON_VERSION="v0.1.1"
+CXX_COMMON_VERSION="v0.1.3"
 
 # There are pre-build versions of various libraries for specific
 # Ubuntu releases.
@@ -175,15 +175,15 @@ function DownloadLibraries
     #BUILD_FLAGS="${BUILD_FLAGS} -DCMAKE_OSX_SYSROOT=${sdk_root}"
     # Min version supported
     OS_VERSION="macos-10.15"
-    XCODE_VERSION="12.1.0"
+    # Hard-coded to match pre-built binaries in CI
+    XCODE_VERSION="12.4"
     if [[ "$(sw_vers -productVersion)" == "10.15"* ]]; then
       echo "Found MacOS Catalina"
       OS_VERSION="macos-10.15"
-      XCODE_VERSION="12.1.0"
-    elif [[ "$(sw_vers -productVersion)" == "11.0"* ]]; then
+    elif [[ "$(sw_vers -productVersion)" == "11."* ]]; then
       echo "Found MacOS Big Sur"
-      OS_VERSION="macos-11.0"
-      XCODE_VERSION="12.2.0"
+      # Uses 10.15 binaries
+      OS_VERSION="macos-10.15"
     else
       echo "WARNING: ****Likely unsupported MacOS Version****"
       echo "WARNING: ****Using ${OS_VERSION}****"
