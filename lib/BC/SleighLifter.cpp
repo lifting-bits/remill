9f90e8f129ad8378eba44f1ba1f6d25d69e7c118
2over12 <ian.smith@trailofbits.com>
2022-03-28 11:27:47 -0400

    relift bytes

--- lib/BC/SleighLifter.cpp
+++ lib/BC/SleighLifter.cpp
@@ -2,6 +2,33 @@
 #include <remill/BC/SleighLifter.h>
 namespace remill {
 
+
+namespace {
+
+class PcodeToLLVMEmitIntoBlock : public PcodeEmit {
+ private:
+  llvm::BasicBlock *target_block;
+  llvm::Value *state_pointer;
+  const Instruction &insn;
+  LiftStatus status;
+
+ public:
+  PcodeToLLVMEmitIntoBlock(llvm::BasicBlock *target_block,
+                           llvm::Value *state_pointer, const Instruction &insn)
+      : target_block(target_block),
+        state_pointer(state_pointer),
+        insn(insn),
+        status(remill::LiftStatus::kLiftedInvalidInstruction){};
+
+  void dump(const Address &addr, OpCode opc, VarnodeData *outvar,
+            VarnodeData *vars, int4 isize) override {}
+
+  LiftStatus GetStatus() {
+    return this->status;
+  }
+};
+}  // namespace
+
 LiftStatus
 SleighLifter::LiftIntoBlock(Instruction &inst, llvm::BasicBlock *block,
                             llvm::Value *state_ptr, bool is_delayed) {
@@ -12,90 +39,12 @@ SleighLifter::LiftIntoBlock(Instruction &inst, llvm::BasicBlock *block,
     return kLiftedInvalidInstruction;
   }
 
-  llvm::IRBuilder<> ir(block);
+  PcodeToLLVMEmitIntoBlock lifter(block, state_ptr, inst);
+  auto res = this->sleigh_context.oneInstruction(lifter, inst.bytes);
 
-  const OpCode op = get_opcode(inst.function);
-  LOG(INFO) << "Attempting to lift opcode: " << op;
-  switch (op) {
-    case OpCode::CPUI_BOOL_NEGATE:
-      return LiftUnOp(inst, block, state_ptr, ir, op);
-    case CPUI_INT_LESS:
-    case CPUI_INT_SLESS:
-    case CPUI_INT_EQUAL:
-    case CPUI_INT_SUB:
-    case CPUI_INT_SBORROW:
-    case CPUI_INT_AND: return LiftBinOp(inst, block, state_ptr, ir, op); break;
-    case CPUI_POPCOUNT: LiftPopCount(inst, block, state_ptr, ir); break;
-    default: LOG(ERROR) << "Unsupported p-code opcode " << inst.function; break;
-  }
+  //NOTE(Ian): If we made it past decoding we should be able to decode the bytes again
+  assert(res.has_value());
 
-  return kLiftedInstruction;
-}
-
-LiftStatus SleighLifter::LiftBinOp(Instruction &inst, llvm::BasicBlock *block,
-                                   llvm::Value *state_ptr,
-                                   llvm::IRBuilder<> &ir, OpCode op) {
-  if (inst.operands.size() != 3) {
-    LOG(ERROR) << "Unexpected number of operands: " << inst.operands.size();
-    inst.operands.clear();
-    return kLiftedInvalidInstruction;
-  }
-  // We want something like `InstructionLifter::LiftOperand` but without the need for an
-  // `llvm::Argument` pointer since we're not calling a function in the runtime.
-  //
-  // TODO(alex): This doesn't handle NULL arguments. Refactor to allow us to use the operand
-  // lifting without having an argument.
-  llvm::Value *out_val =
-      LiftOperand(inst, block, state_ptr, nullptr, inst.operands[0]);
-  llvm::Value *lhs_val =
-      LiftOperand(inst, block, state_ptr, nullptr, inst.operands[1]);
-  llvm::Value *rhs_val =
-      LiftOperand(inst, block, state_ptr, nullptr, inst.operands[2]);
-
-  llvm::Value *bin_op_val = nullptr;
-  switch (op) {
-    case CPUI_INT_LESS: bin_op_val = ir.CreateICmpULT(lhs_val, rhs_val); break;
-    case CPUI_INT_SLESS: bin_op_val = ir.CreateICmpULT(lhs_val, rhs_val); break;
-    case CPUI_INT_EQUAL: bin_op_val = ir.CreateICmpEQ(lhs_val, rhs_val); break;
-    case CPUI_INT_SUB: bin_op_val = ir.CreateSub(lhs_val, rhs_val); break;
-    case CPUI_INT_SBORROW: bin_op_val = ir.CreateURem(lhs_val, rhs_val); break;
-
-    case CPUI_INT_AND: bin_op_val = ir.CreateAnd(lhs_val, rhs_val); break;
-    default: LOG(ERROR) << "Invalid binary op " << get_opname(op); break;
-  }
-
-  // Assign the out variable to the result of the binary operation
-  ir.CreateStore(bin_op_val, out_val);
-  return kLiftedInstruction;
-}
-
-LiftStatus SleighLifter::LiftUnOp(Instruction &inst, llvm::BasicBlock *block,
-                                  llvm::Value *state_ptr, llvm::IRBuilder<> &ir,
-                                  OpCode op) {
-  if (inst.operands.size() != 2) {
-    LOG(ERROR) << "Unexpected number of operands";
-    inst.operands.clear();
-    return kLiftedInvalidInstruction;
-  }
-
-  llvm::Value *out_val =
-      LiftOperand(inst, block, state_ptr, nullptr, inst.operands[0]);
-  llvm::Value *operand =
-      LiftOperand(inst, block, state_ptr, nullptr, inst.operands[1]);
-
-
-  llvm::Value *unop_val = nullptr;
-  switch (op) {
-    default: LOG(ERROR) << "Invalid unary op " << get_opname(op); break;
-  }
-
-  // Assign the out variable to the result of the binary operation
-  ir.CreateStore(unop_val, out_val);
-  return kLiftedInstruction;
-}
-
-void SleighLifter::LiftPopCount(Instruction &inst, llvm::BasicBlock *block,
-                                llvm::Value *state_ptr, llvm::IRBuilder<> &ir) {
-  // TODO(alex): Call CTPOP LLVM intrinsic
+  return lifter.GetStatus();
 }
 }  // namespace remill
