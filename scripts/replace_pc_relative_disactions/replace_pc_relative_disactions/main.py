32c1ef2caa29c99c4c3aad6911c0f0a213dd7622
2over12 <ian.smith@trailofbits.com>
2022-06-28 13:29:42 -0400

    handle duplicate names in same constructor

--- scripts/replace_pc_relative_disactions/replace_pc_relative_disactions/main.py
+++ scripts/replace_pc_relative_disactions/replace_pc_relative_disactions/main.py
@@ -5,7 +5,7 @@ import os
 import tempfile
 import subprocess
 
-CONNECTIVES_WITHOUT_SEMICOLON = "&|"
+CONNECTIVES_WITHOUT_SEMICOLON = "&|."
 CONNECTIVES = CONNECTIVES_WITHOUT_SEMICOLON+r";"
 
 OPERATORS = r"*+\>\<\(\)$"
@@ -40,21 +40,37 @@ CONSTRUCTOR_BASE_REGEX = r"(?P<table_name>[\w]*):" + DISPLAY_SECTION + \
 class Context:
     def __init__(self) -> None:
         self.program_counter = "$(INST_NEXT_PTR)"
+        self.name_ctr = 0
 
 
 class Environment:
-    def __init__(self, program_counter: str, size_hint: str) -> None:
+    def __init__(self, cont: Context, size_hint: str) -> None:
         self.names_to_calculating_expression: Dict[str, str] = {}
-        self.program_counter = program_counter
+        self.definition_statements: Dict[str, str] = {}
+        self.cont = cont
         self.size_hint = size_hint
         self.handle_inst_next_statement("inst_next=inst_next")
 
     def prepare_statement(self, name: str, exp: str):
-        replaced_exp = exp.replace("inst_next", self.program_counter)
-        return f"remill_please_dont_use_this_temp_name:{self.size_hint}={name};\nclaim_eq(remill_please_dont_use_this_temp_name,{replaced_exp})"
+        replaced_exp = exp.replace("inst_next", self.cont.program_counter)
+        if name not in self.names_to_calculating_expression:
+            self.cont.name_ctr += 1
+            definer = f"remill_please_dont_use_this_temp_name{self.cont.name_ctr:x}:{self.size_hint}={name}"
+            claim = f"claim_eq(remill_please_dont_use_this_temp_name{self.cont.name_ctr:x}, {replaced_exp})"
+            self.names_to_calculating_expression[name] = claim
+            self.definition_statements[name] = definer
 
     def get_priors(self, stat: str) -> List[str]:
         tot = []
+        to_remove = []
+        for k, v in self.definition_statements.items():
+            if k in stat:
+                tot.append(v)
+                to_remove.append(k)
+
+        for r in to_remove:
+            del self.definition_statements[r]
+
         for k, v in self.names_to_calculating_expression.items():
             if k in stat:
                 tot.append(v)
@@ -64,8 +80,7 @@ class Environment:
         if "=" in stat:
             name, exp = stat.split("=", 1)
             if "inst_next" in exp:
-                self.names_to_calculating_expression[name] = self.prepare_statement(
-                    name, exp)
+                self.prepare_statement(name, exp)
 
 
 def build_constructor(env: Environment, constructor: Match[str]) -> Optional[str]:
@@ -125,7 +140,7 @@ def main():
                 for constructor in construct_pat.finditer(target):
                     total_output += constructor.string[last_offset:constructor.start()]
                     last_offset = constructor.end()
-                    env = Environment(cont.program_counter,
+                    env = Environment(cont,
                                       args.inst_next_size_hint)
                     act_section = constructor.group("action_section")
                     if act_section:
