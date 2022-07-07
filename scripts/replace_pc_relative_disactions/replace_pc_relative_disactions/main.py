46cf25cc164c72867ce509b77543b88722dbaea3
2over12 <ian.smith@trailofbits.com>
2022-07-07 15:37:18 -0400

    handle inst_start in patches

--- scripts/replace_pc_relative_disactions/replace_pc_relative_disactions/main.py
+++ scripts/replace_pc_relative_disactions/replace_pc_relative_disactions/main.py
@@ -1,9 +1,10 @@
 import argparse
 import re
-from typing import Dict, List, Match, Optional
+from typing import Dict, List, Match, Optional, Set, Tuple
 import os
 import tempfile
 import subprocess
+import abc
 
 CONNECTIVES_WITHOUT_SEMICOLON = "&|.!"
 CONNECTIVES = CONNECTIVES_WITHOUT_SEMICOLON+r";"
@@ -29,8 +30,8 @@ DISASSEMBLY_ACTION_SECTION = r"(?P<action_section>\[" + \
 SEMANTICS_STATEMENT = "(?P<statement>[\s\w" + \
     PCODE_SPECIFIC_OPERATORS+OPERATORS+CONNECTIVES_WITHOUT_SEMICOLON+"+]*;)"
 
-SEMANTICS_SECTION = r"\s*[{]\s*(?P<semantics>\s" + \
-    SEMANTICS_STATEMENT+r"*\s*)\s*[}]\s*"
+SEMANTICS_SECTION = r"\s*(?P<complete_semantics_section>[{]\s*(?P<semantics>\s" + \
+    SEMANTICS_STATEMENT+r"*\s*)\s*[}])\s*"
 
 CONSTRUCTOR_BASE_REGEX = r"(?P<table_name>[\w]*):" + DISPLAY_SECTION + \
     GENERIC_CHARACTER_GROUP_WITH_EQUALS + r"*" + \
@@ -39,26 +40,91 @@ CONSTRUCTOR_BASE_REGEX = r"(?P<table_name>[\w]*):" + DISPLAY_SECTION + \
 
 class Context:
     def __init__(self) -> None:
-        self.program_counter = "$(INST_NEXT_PTR)"
         self.name_ctr = 0
 
 
+REMILL_INSN_SIZE_NAME = "remill_insn_size"
+
+
+class ExpressionReplacer(abc.ABC):
+    def __init__(self) -> None:
+        self.program_counter = "$(INST_NEXT_PTR)"
+        super().__init__()
+
+    @abc.abstractmethod
+    def applies_to_expression(self, exp: str) -> bool:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def generate_unfolded_exp(self, exp: str) -> str:
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def required_invisible_operands(self) -> List[str]:
+        raise NotImplementedError
+
+
+class InstStartReplacer(ExpressionReplacer):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def applies_to_expression(self, exp: str) -> bool:
+        return "inst_start" in exp
+
+    def generate_unfolded_exp(self, exp: str) -> str:
+        return exp.replace("inst_start", f"({self.program_counter}-{REMILL_INSN_SIZE_NAME})")
+
+    @property
+    def required_invisible_operands(self) -> List[str]:
+        return [REMILL_INSN_SIZE_NAME]
+
+
+class InstNextReplacer(ExpressionReplacer):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def applies_to_expression(self, exp: str) -> bool:
+        return "inst_next" in exp
+
+    def generate_unfolded_exp(self, exp: str) -> str:
+        return exp.replace("inst_next", self.program_counter)
+
+    @property
+    def required_invisible_operands(self) -> List[str]:
+        return []
+
+
 class Environment:
-    def __init__(self, cont: Context, size_hint: str) -> None:
+    def __init__(self, cont: Context, size_hint: str, replacements: List[ExpressionReplacer]) -> None:
+        self.name_to_invisible_variables: Dict[str, Set[str]] = {}
         self.names_to_calculating_expression: Dict[str, str] = {}
         self.definition_statements: Dict[str, str] = {}
         self.cont = cont
         self.size_hint = size_hint
+        self.replacements = replacements
         self.handle_inst_next_statement("inst_next=inst_next")
+        self.handle_inst_next_statement("inst_start=inst_start")
 
-    def prepare_statement(self, name: str, exp: str):
-        replaced_exp = exp.replace("inst_next", self.cont.program_counter)
+    def prepare_statement(self, repl: ExpressionReplacer,  name: str, exp: str):
+        replaced_exp = repl.generate_unfolded_exp(exp)
         if name not in self.names_to_calculating_expression:
+            for k in repl.required_invisible_operands:
+                self.name_to_invisible_variables.setdefault(name, set()).add(k)
             self.cont.name_ctr += 1
             definer = f"remill_please_dont_use_this_temp_name{self.cont.name_ctr:x}:{self.size_hint}={name}"
             claim = f"claim_eq(remill_please_dont_use_this_temp_name{self.cont.name_ctr:x}, {replaced_exp})"
             self.names_to_calculating_expression[name] = claim
             self.definition_statements[name] = definer
+
+    def get_required_invisible_operands(self, statements: List[str]) -> List[str]:
+        tot_reqs = set()
+        for (expr_name, action_name) in self.name_to_invisible_variables.items():
+            for stat in statements:
+                if expr_name in stat:
+                    tot_reqs.update(action_name)
+
+        return list(tot_reqs)
 
     def get_priors(self, stat: str) -> List[str]:
         tot = []
@@ -79,16 +145,37 @@ class Environment:
     def handle_inst_next_statement(self, stat: str):
         if "=" in stat:
             name, exp = stat.split("=", 1)
-            if "inst_next" in exp:
-                self.prepare_statement(name, exp)
+
+            matching_replacers = [
+                exp_repr for exp_repr in self.replacements if exp_repr.applies_to_expression(exp)]
+
+            if len(matching_replacers) > 1:
+                raise RuntimeError(
+                    f"Conflicting replacers for expression: {exp}")
+            if len(matching_replacers) == 1:
+                self.prepare_statement(matching_replacers[0], name, exp)
 
 
 def build_constructor(env: Environment, constructor: Match[str]) -> Optional[str]:
     semantics_section = constructor.group("semantics")
+
+    statements = semantics_section.split(";")
+    invis_ops = env.get_required_invisible_operands(statements)
+
+    invisible_operand_list = "; "+"; ".join(invis_ops)
+    if len(invis_ops) <= 0:
+        invisible_operand_list = ""
+    # check if we have an action section if we dont add one/ otherwise
+    # action section ends at "]"
+    has_action_section = constructor.group("action_section") is not None
+    new_act_section = constructor.group(
+        "action_section") if has_action_section else ""
+
     cons_start = constructor.start()
     cons_end = constructor.end()
-    sem_start = constructor.start("semantics")
-    sem_end = constructor.end("semantics")
+    replaced_start = constructor.start(
+        "action_section") if has_action_section else constructor.start("complete_semantics_section")
+    replaced_end = constructor.end("complete_semantics_section")
     new_section = []
     used_priors = False
     for stat in semantics_section.split(";"):
@@ -102,7 +189,8 @@ def build_constructor(env: Environment, constructor: Match[str]) -> Optional[str
 
     str_sec = ";\n".join(new_section)
 
-    return f"{constructor.string[cons_start:sem_start]}\n{str_sec}\n{constructor.string[sem_end:cons_end]}"
+    # print(replaced_cons)
+    return f"{constructor.string[cons_start:replaced_start]} {invisible_operand_list} {new_act_section} {{ \n{str_sec}  }}\n {constructor.string[replaced_end:cons_end]}"
 
 
 ENDIAN_DEF_REGEX = "define\s*endian\s*=[\w()$]+;"
@@ -137,11 +225,18 @@ def main():
 
                 last_offset = endian_def.end()
                 cont = Context()
+                firstConstructor = True
                 for constructor in construct_pat.finditer(target):
                     total_output += constructor.string[last_offset:constructor.start()]
+
+                    if firstConstructor:
+                        # make sure we define instruction size before first cons.
+                        total_output += f"\n{REMILL_INSN_SIZE_NAME}: calculated_size is epsilon [calculated_size= inst_next-inst_start; ] {{export calculated_size; }}\n\n"
+                        firstConstructor = False
+
                     last_offset = constructor.end()
                     env = Environment(cont,
-                                      args.inst_next_size_hint)
+                                      args.inst_next_size_hint, [InstStartReplacer(), InstNextReplacer()])
                     act_section = constructor.group("action_section")
                     if act_section:
                         statements = act_section[
