1a179a5149dd01a80326c3669d7fd6959823c11c
2over12 <ian.smith@trailofbits.com>
2022-07-12 14:18:09 -0400

    remove duplicates

--- scripts/diff_tester_export_insns/diff_tester_export_insns/run.py
+++ scripts/diff_tester_export_insns/diff_tester_export_insns/run.py
@@ -13,12 +13,15 @@ def main():
     arch = target_bin_view.arch
     mx_insn_len = arch.max_instr_length
     insn_testcase_list = []
+    seen_bytes = set()
     for func in target_bin_view.functions:
         for (_, start_addr) in func.instructions:
             buff = target_bin_view[start_addr: start_addr+mx_insn_len]
             insn_size = arch.get_instruction_info(buff, start_addr).length
             final_bytes = buff[:insn_size]
-            insn_testcase_list.append((start_addr, final_bytes.hex()))
+            if final_bytes not in seen_bytes:
+                insn_testcase_list.append((start_addr, final_bytes.hex()))
+            seen_bytes.add(final_bytes)
 
     with open(args.out, "w") as f:
         json.dump(insn_testcase_list, f, indent=4)
