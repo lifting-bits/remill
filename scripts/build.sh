3056b3bcb4bb4346c55f36b90e9b19b9bd70a43f
William Tan <Ninja3047@users.noreply.github.com>
2022-08-29 13:59:22 -0400

    extra v

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,7 +27,7 @@ LLVM_VERSION=llvm-14
 OS_VERSION=
 ARCH_VERSION=
 BUILD_FLAGS=
-CXX_COMMON_VERSION="v0.2.10"
+CXX_COMMON_VERSION="0.2.10"
 
 # There are pre-build versions of various libraries for specific
 # Ubuntu releases.
