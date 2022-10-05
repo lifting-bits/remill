d0d60e42d56ae5e42249344db557addbbe230350
2over12 <ian.smith@trailofbits.com>
2022-10-05 15:35:04 -0400

    add more debug

--- lib/BC/SleighLifter.cpp
+++ lib/BC/SleighLifter.cpp
@@ -17,6 +17,7 @@
 #include <glog/logging.h>
 #include <lib/Arch/Sleigh/Arch.h>
 #include <lib/Arch/Sleigh/ControlFlowStructuring.h>
+#include <llvm/ADT/StringExtras.h>
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Value.h>
 #include <remill/BC/ABI.h>
@@ -305,7 +306,9 @@ class SleighLifter::PcodeToLLVMEmitIntoBlock : public PcodeEmit {
   void UpdateStatus(LiftStatus new_status, OpCode opc) {
     if (new_status != LiftStatus::kLiftedInstruction) {
       this->status = new_status;
-      DLOG(ERROR) << "Failed to lift insn with opcode: " << get_opname(opc);
+      DLOG(ERROR) << "Failed to lift insn with opcode: " << get_opname(opc)
+                  << " in insn: " << std::hex << this->insn.pc
+                  << llvm::toHex(this->insn.bytes);
     }
   }
 
