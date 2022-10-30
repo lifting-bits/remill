1509edbe292abe36c595295b9d8d9e9d6b12ac7c
Alex Cameron <asc@tetsuo.sh>
2022-10-30 19:05:14 +1100

    Bump CXX Common version

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,7 +27,7 @@ LLVM_VERSION=llvm-15
 OS_VERSION=
 ARCH_VERSION=
 BUILD_FLAGS=
-CXX_COMMON_VERSION="0.2.10"
+CXX_COMMON_VERSION="0.2.12"
 
 # There are pre-build versions of various libraries for specific
 # Ubuntu releases.
