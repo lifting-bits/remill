7f091d4256060c254fcd15f35fd8b9bd6abd157f
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-07-17 18:32:00 +0200

    Remove [[gnu::const]] (readnone) from __remill intrinsics (#710)
    * Remove [[gnu::const]] (readnone) from __remill intrinsics
    
    * Remove FindPureIntrinsic and SetMemoryReadNone

--- lib/BC/IntrinsicTable.cpp
+++ lib/BC/IntrinsicTable.cpp
@@ -51,28 +51,6 @@ static llvm::Function *FindIntrinsic(llvm::Module *module, const char *name) {
   return function;
 }
 
-// Find a specific function.
-static llvm::Function *FindPureIntrinsic(llvm::Module *module,
-                                         const char *name) {
-  auto function = FindIntrinsic(module, name);
-
-  // We want memory intrinsics to be marked as not accessing memory so that
-  // they don't interfere with dead store elimination.
-#if LLVM_VERSION_NUMBER < LLVM_VERSION(16, 0)
-  function->addFnAttr(llvm::Attribute::ReadNone);
-#else
-  // In LLVM 16, `readnone` has been replaced by `memory(none)`.
-  function->setDoesNotAccessMemory();
-#endif
-  return function;
-}
-
-static llvm::Function *SetMemoryReadNone(llvm::Function *func) {
-  auto arg = remill::NthArgument(func, 0);
-  arg->addAttr(llvm::Attribute::ReadNone);
-  return func;
-}
-
 }  // namespace
 
 IntrinsicTable::IntrinsicTable(llvm::Module *module)
@@ -89,40 +67,40 @@ IntrinsicTable::IntrinsicTable(llvm::Module *module)
       async_hyper_call(FindIntrinsic(module, "__remill_async_hyper_call")),
 
       // Memory access.
-      read_memory_8(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_8"))),
-      read_memory_16(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_16"))),
-      read_memory_32(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_32"))),
-      read_memory_64(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_64"))),
+      read_memory_8(FindIntrinsic(module, "__remill_read_memory_8")),
+      read_memory_16(FindIntrinsic(module, "__remill_read_memory_16")),
+      read_memory_32(FindIntrinsic(module, "__remill_read_memory_32")),
+      read_memory_64(FindIntrinsic(module, "__remill_read_memory_64")),
 
-      write_memory_8(FindPureIntrinsic(module, "__remill_write_memory_8")),
-      write_memory_16(FindPureIntrinsic(module, "__remill_write_memory_16")),
-      write_memory_32(FindPureIntrinsic(module, "__remill_write_memory_32")),
-      write_memory_64(FindPureIntrinsic(module, "__remill_write_memory_64")),
+      write_memory_8(FindIntrinsic(module, "__remill_write_memory_8")),
+      write_memory_16(FindIntrinsic(module, "__remill_write_memory_16")),
+      write_memory_32(FindIntrinsic(module, "__remill_write_memory_32")),
+      write_memory_64(FindIntrinsic(module, "__remill_write_memory_64")),
 
-      read_memory_f32(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_f32"))),
-      read_memory_f64(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_f64"))),
-      read_memory_f80(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_f80"))),
-      read_memory_f128(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_read_memory_f128"))),
+      read_memory_f32(FindIntrinsic(module, "__remill_read_memory_f32")),
+      read_memory_f64(FindIntrinsic(module, "__remill_read_memory_f64")),
+      read_memory_f80(FindIntrinsic(module, "__remill_read_memory_f80")),
+      read_memory_f128(FindIntrinsic(module, "__remill_read_memory_f128")),
 
-      write_memory_f32(FindPureIntrinsic(module, "__remill_write_memory_f32")),
-      write_memory_f64(FindPureIntrinsic(module, "__remill_write_memory_f64")),
-      write_memory_f80(FindPureIntrinsic(module, "__remill_write_memory_f80")),
+      write_memory_f32(FindIntrinsic(module, "__remill_write_memory_f32")),
+      write_memory_f64(FindIntrinsic(module, "__remill_write_memory_f64")),
+      write_memory_f80(FindIntrinsic(module, "__remill_write_memory_f80")),
       write_memory_f128(
-          FindPureIntrinsic(module, "__remill_write_memory_f128")),
+          FindIntrinsic(module, "__remill_write_memory_f128")),
 
       // Memory barriers.
       barrier_load_load(
-          FindPureIntrinsic(module, "__remill_barrier_load_load")),
+          FindIntrinsic(module, "__remill_barrier_load_load")),
       barrier_load_store(
-          FindPureIntrinsic(module, "__remill_barrier_load_store")),
+          FindIntrinsic(module, "__remill_barrier_load_store")),
       barrier_store_load(
-          FindPureIntrinsic(module, "__remill_barrier_store_load")),
+          FindIntrinsic(module, "__remill_barrier_store_load")),
       barrier_store_store(
-          FindPureIntrinsic(module, "__remill_barrier_store_store")),
-      atomic_begin(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_atomic_begin"))),
-      atomic_end(SetMemoryReadNone(FindPureIntrinsic(module, "__remill_atomic_end"))),
-      delay_slot_begin(FindPureIntrinsic(module, "__remill_delay_slot_begin")),
-      delay_slot_end(FindPureIntrinsic(module, "__remill_delay_slot_end")),
+          FindIntrinsic(module, "__remill_barrier_store_store")),
+      atomic_begin(FindIntrinsic(module, "__remill_atomic_begin")),
+      atomic_end(FindIntrinsic(module, "__remill_atomic_end")),
+      delay_slot_begin(FindIntrinsic(module, "__remill_delay_slot_begin")),
+      delay_slot_end(FindIntrinsic(module, "__remill_delay_slot_end")),
 
       //      // Optimization guides.
       //      //
@@ -131,28 +109,28 @@ IntrinsicTable::IntrinsicTable(llvm::Module *module)
       //      defer_inlining(FindIntrinsic(module, "__remill_defer_inlining")),
 
       // Optimization enablers.
-      undefined_8(FindPureIntrinsic(module, "__remill_undefined_8")),
-      undefined_16(FindPureIntrinsic(module, "__remill_undefined_16")),
-      undefined_32(FindPureIntrinsic(module, "__remill_undefined_32")),
-      undefined_64(FindPureIntrinsic(module, "__remill_undefined_64")),
-      undefined_f32(FindPureIntrinsic(module, "__remill_undefined_f32")),
-      undefined_f64(FindPureIntrinsic(module, "__remill_undefined_f64")),
-      undefined_f80(FindPureIntrinsic(module, "__remill_undefined_f80")),
+      undefined_8(FindIntrinsic(module, "__remill_undefined_8")),
+      undefined_16(FindIntrinsic(module, "__remill_undefined_16")),
+      undefined_32(FindIntrinsic(module, "__remill_undefined_32")),
+      undefined_64(FindIntrinsic(module, "__remill_undefined_64")),
+      undefined_f32(FindIntrinsic(module, "__remill_undefined_f32")),
+      undefined_f64(FindIntrinsic(module, "__remill_undefined_f64")),
+      undefined_f80(FindIntrinsic(module, "__remill_undefined_f80")),
 
       // Flag computations
       flag_computation_zero(
-          FindPureIntrinsic(module, "__remill_flag_computation_zero")),
+          FindIntrinsic(module, "__remill_flag_computation_zero")),
       flag_computation_sign(
-          FindPureIntrinsic(module, "__remill_flag_computation_sign")),
+          FindIntrinsic(module, "__remill_flag_computation_sign")),
       flag_computation_overflow(
-          FindPureIntrinsic(module, "__remill_flag_computation_overflow")),
+          FindIntrinsic(module, "__remill_flag_computation_overflow")),
       flag_computation_carry(
-          FindPureIntrinsic(module, "__remill_flag_computation_carry")),
+          FindIntrinsic(module, "__remill_flag_computation_carry")),
       // compares
-      compare_sle(FindPureIntrinsic(module, "__remill_compare_sle")),
-      compare_sgt(FindPureIntrinsic(module, "__remill_compare_sgt")),
-      compare_eq(FindPureIntrinsic(module, "__remill_compare_eq")),
-      compare_neq(FindPureIntrinsic(module, "__remill_compare_neq")),
+      compare_sle(FindIntrinsic(module, "__remill_compare_sle")),
+      compare_sgt(FindIntrinsic(module, "__remill_compare_sgt")),
+      compare_eq(FindIntrinsic(module, "__remill_compare_eq")),
+      compare_neq(FindIntrinsic(module, "__remill_compare_neq")),
 
       lifted_function_type(error->getFunctionType()),
       state_ptr_type(llvm::dyn_cast<llvm::PointerType>(
