71a01705821b21ee2add7c03df032ed12d76c34c
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-07-17 16:20:10 +0200

    Remove [[gnu::const]] (readnone) from __remill intrinsics

--- include/remill/Arch/Runtime/Intrinsics.h
+++ include/remill/Arch/Runtime/Intrinsics.h
@@ -22,47 +22,40 @@
 extern "C" {
 
 // Memory read intrinsics.
-[[gnu::used, gnu::const]] extern uint8_t __remill_read_memory_8(Memory *,
-                                                                addr_t);
+[[gnu::used]] extern uint8_t __remill_read_memory_8(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern uint16_t __remill_read_memory_16(Memory *,
-                                                                  addr_t);
+[[gnu::used]] extern uint16_t __remill_read_memory_16(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern uint32_t __remill_read_memory_32(Memory *,
-                                                                  addr_t);
+[[gnu::used]] extern uint32_t __remill_read_memory_32(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern uint64_t __remill_read_memory_64(Memory *,
-                                                                  addr_t);
+[[gnu::used]] extern uint64_t __remill_read_memory_64(Memory *, addr_t);
 
 // Memory write intrinsics.
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_memory_8(Memory *, addr_t, uint8_t);
+[[gnu::used]] extern Memory *__remill_write_memory_8(Memory *, addr_t, uint8_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_memory_16(Memory *, addr_t, uint16_t);
+[[gnu::used]] extern Memory *__remill_write_memory_16(Memory *, addr_t,
+                                                      uint16_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_memory_32(Memory *, addr_t, uint32_t);
+[[gnu::used]] extern Memory *__remill_write_memory_32(Memory *, addr_t,
+                                                      uint32_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_memory_64(Memory *, addr_t, uint64_t);
+[[gnu::used]] extern Memory *__remill_write_memory_64(Memory *, addr_t,
+                                                      uint64_t);
 
-[[gnu::used, gnu::const]] extern float32_t __remill_read_memory_f32(Memory *,
-                                                                    addr_t);
+[[gnu::used]] extern float32_t __remill_read_memory_f32(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern float64_t __remill_read_memory_f64(Memory *,
-                                                                    addr_t);
+[[gnu::used]] extern float64_t __remill_read_memory_f64(Memory *, addr_t);
 
 [[gnu::used]] extern Memory *__remill_read_memory_f80(Memory *, addr_t,
                                                       native_float80_t &);
 
 [[gnu::used]] extern float128_t __remill_read_memory_f128(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_memory_f32(Memory *, addr_t, float32_t);
+[[gnu::used]] extern Memory *__remill_write_memory_f32(Memory *, addr_t,
+                                                       float32_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_memory_f64(Memory *, addr_t, float64_t);
+[[gnu::used]] extern Memory *__remill_write_memory_f64(Memory *, addr_t,
+                                                       float64_t);
 
 [[gnu::used]] extern Memory *
 __remill_write_memory_f80(Memory *, addr_t, const native_float80_t &);
@@ -70,54 +63,50 @@ __remill_write_memory_f80(Memory *, addr_t, const native_float80_t &);
 [[gnu::used]] extern Memory *__remill_write_memory_f128(Memory *, addr_t,
                                                         float128_t);
 
-[[gnu::used, gnu::const]] extern uint8_t __remill_undefined_8(void);
+[[gnu::used]] extern uint8_t __remill_undefined_8(void);
 
-[[gnu::used, gnu::const]] extern uint16_t __remill_undefined_16(void);
+[[gnu::used]] extern uint16_t __remill_undefined_16(void);
 
-[[gnu::used, gnu::const]] extern uint32_t __remill_undefined_32(void);
+[[gnu::used]] extern uint32_t __remill_undefined_32(void);
 
-[[gnu::used, gnu::const]] extern uint64_t __remill_undefined_64(void);
+[[gnu::used]] extern uint64_t __remill_undefined_64(void);
 
-[[gnu::used, gnu::const]] extern float32_t __remill_undefined_f32(void);
+[[gnu::used]] extern float32_t __remill_undefined_f32(void);
 
-[[gnu::used, gnu::const]] extern float64_t __remill_undefined_f64(void);
+[[gnu::used]] extern float64_t __remill_undefined_f64(void);
 
-[[gnu::used, gnu::const]] extern float80_t __remill_undefined_f80(void);
+[[gnu::used]] extern float80_t __remill_undefined_f80(void);
 
-[[gnu::used, gnu::const]] extern float128_t __remill_undefined_f128(void);
+[[gnu::used]] extern float128_t __remill_undefined_f128(void);
 
-[[gnu::used, gnu::const]] extern bool
-__remill_flag_computation_zero(bool result, ...);
+[[gnu::used]] extern bool __remill_flag_computation_zero(bool result, ...);
 
-[[gnu::used, gnu::const]] extern bool
-__remill_flag_computation_sign(bool result, ...);
+[[gnu::used]] extern bool __remill_flag_computation_sign(bool result, ...);
 
-[[gnu::used, gnu::const]] extern bool
-__remill_flag_computation_overflow(bool result, ...);
+[[gnu::used]] extern bool __remill_flag_computation_overflow(bool result, ...);
 
-[[gnu::used, gnu::const]] extern bool
-__remill_flag_computation_carry(bool result, ...);
+[[gnu::used]] extern bool __remill_flag_computation_carry(bool result, ...);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_sle(bool result);
+[[gnu::used]] extern bool __remill_compare_sle(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_slt(bool result);
+[[gnu::used]] extern bool __remill_compare_slt(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_sge(bool result);
+[[gnu::used]] extern bool __remill_compare_sge(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_sgt(bool result);
+[[gnu::used]] extern bool __remill_compare_sgt(bool result);
 
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_ule(bool result);
+[[gnu::used]] extern bool __remill_compare_ule(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_ult(bool result);
+[[gnu::used]] extern bool __remill_compare_ult(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_ugt(bool result);
+[[gnu::used]] extern bool __remill_compare_ugt(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_uge(bool result);
+[[gnu::used]] extern bool __remill_compare_uge(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_eq(bool result);
+[[gnu::used]] extern bool __remill_compare_eq(bool result);
 
-[[gnu::used, gnu::const]] extern bool __remill_compare_neq(bool result);
+[[gnu::used]] extern bool __remill_compare_neq(bool result);
 
 // Generic error.
 [[gnu::used]] extern Memory *__remill_error(State &, addr_t addr, Memory *);
@@ -152,41 +141,27 @@ __remill_sync_hyper_call(State &, Memory *, SyncHyperCall::Name);
 //  http://g.oswego.edu/dl/jmm/cookbook.html
 //  http://preshing.com/20120913/acquire-and-release-semantics/
 //  http://preshing.com/20120710/memory-barriers-are-like-source-control-operations/
-[[gnu::used, gnu::const]] extern Memory *__remill_barrier_load_load(Memory *);
+[[gnu::used]] extern Memory *__remill_barrier_load_load(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
+[[gnu::used]] extern Memory *
 __remill_barrier_load_store(Memory *);  // Load acquire.
 
-[[gnu::used, gnu::const]] extern Memory *__remill_barrier_store_load(Memory *);
+[[gnu::used]] extern Memory *__remill_barrier_store_load(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
+[[gnu::used]] extern Memory *
 __remill_barrier_store_store(Memory *);  // Store release.
 
 // Atomic operations. The address/size are hints, but the granularity of the
 // access can be bigger. These have implicit StoreLoad semantics.
-[[gnu::used, gnu::const]] extern Memory *__remill_atomic_begin(Memory *);
+[[gnu::used]] extern Memory *__remill_atomic_begin(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_atomic_end(Memory *);
+[[gnu::used]] extern Memory *__remill_atomic_end(Memory *);
 
 // Used to signal the begin/ending of an instruction executed within a delay
 // slot.
-[[gnu::used, gnu::const]] extern Memory *__remill_delay_slot_begin(Memory *);
+[[gnu::used]] extern Memory *__remill_delay_slot_begin(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_delay_slot_end(Memory *);
-
-/* Most memory intrinsics are marked as `[[gnu::const]]` which tells the compiler that they
- * do not read/write to memory. This permits LLVM to optimize around the intrinsic, without thinking
- * about it's internals.
- * The meaning of `[[gnu::const]]` is the function will neither read nor write to the memory. In
- * This case the previous value of memory at `addr` needs to be communicated back to the program
- * which will happen by writing back to the refernecs of `expected`. If the function were declared
- * with `[[gnu::const]]`, the compiler is free to assume that the value of `expected` is not changed
- * and it will cause the unwanted behavior.
- *
- * The `gnu::pure` attribute causes the unwanted behaviour and the argument references updated inside
- * the function are not visible in the caller functions
- */
-
+[[gnu::used]] extern Memory *__remill_delay_slot_end(Memory *);
 
 [[gnu::used]] extern Memory *
 __remill_compare_exchange_memory_8(Memory *, addr_t addr, uint8_t &expected,
@@ -291,138 +266,114 @@ __remill_compare_exchange_memory_128(Memory *, addr_t addr, uint128_t &expected,
 //      auto flags = __remill_fpu_exception_test_and_clear(FE_ALL_EXCEPT, 0);
 //
 // These flags are also subject to optimizations
-[[gnu::used, gnu::const]] extern int
-__remill_fpu_exception_test_and_clear(int read_mask, int clear_mask);
+[[gnu::used]] extern int __remill_fpu_exception_test_and_clear(int read_mask,
+                                                               int clear_mask);
 
 // Read/write to I/O ports.
-[[gnu::used, gnu::const]] extern uint8_t __remill_read_io_port_8(Memory *,
-                                                                 addr_t);
+[[gnu::used]] extern uint8_t __remill_read_io_port_8(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern uint16_t __remill_read_io_port_16(Memory *,
-                                                                   addr_t);
+[[gnu::used]] extern uint16_t __remill_read_io_port_16(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern uint32_t __remill_read_io_port_32(Memory *,
-                                                                   addr_t);
+[[gnu::used]] extern uint32_t __remill_read_io_port_32(Memory *, addr_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_io_port_8(Memory *, addr_t, uint8_t);
+[[gnu::used]] extern Memory *__remill_write_io_port_8(Memory *, addr_t,
+                                                      uint8_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_io_port_16(Memory *, addr_t, uint16_t);
+[[gnu::used]] extern Memory *__remill_write_io_port_16(Memory *, addr_t,
+                                                       uint16_t);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_write_io_port_32(Memory *, addr_t, uint32_t);
+[[gnu::used]] extern Memory *__remill_write_io_port_32(Memory *, addr_t,
+                                                       uint32_t);
 
 // More specific hyper calls.
-[[gnu::used, gnu::const]] extern Memory *__remill_x86_set_segment_es(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_segment_es(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_x86_set_segment_ss(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_segment_ss(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_x86_set_segment_ds(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_segment_ds(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_x86_set_segment_fs(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_segment_fs(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_x86_set_segment_gs(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_segment_gs(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_x86_set_debug_reg(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_debug_reg(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_x86_set_control_reg_0(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_control_reg_0(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_x86_set_control_reg_1(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_control_reg_1(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_x86_set_control_reg_2(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_control_reg_2(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_x86_set_control_reg_3(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_control_reg_3(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_x86_set_control_reg_4(Memory *);
+[[gnu::used]] extern Memory *__remill_x86_set_control_reg_4(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_amd64_set_debug_reg(Memory *);
+[[gnu::used]] extern Memory *__remill_amd64_set_debug_reg(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_amd64_set_control_reg_0(Memory *);
+[[gnu::used]] extern Memory *__remill_amd64_set_control_reg_0(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_amd64_set_control_reg_1(Memory *);
+[[gnu::used]] extern Memory *__remill_amd64_set_control_reg_1(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_amd64_set_control_reg_2(Memory *);
+[[gnu::used]] extern Memory *__remill_amd64_set_control_reg_2(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_amd64_set_control_reg_3(Memory *);
+[[gnu::used]] extern Memory *__remill_amd64_set_control_reg_3(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_amd64_set_control_reg_4(Memory *);
+[[gnu::used]] extern Memory *__remill_amd64_set_control_reg_4(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_amd64_set_control_reg_8(Memory *);
+[[gnu::used]] extern Memory *__remill_amd64_set_control_reg_8(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_aarch64_emulate_instruction(Memory *);
+[[gnu::used]] extern Memory *__remill_aarch64_emulate_instruction(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_aarch32_emulate_instruction(Memory *);
+[[gnu::used]] extern Memory *__remill_aarch32_emulate_instruction(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_aarch32_check_not_el2(Memory *);
+[[gnu::used]] extern Memory *__remill_aarch32_check_not_el2(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_sparc_set_asi_register(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_set_asi_register(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_sparc_unimplemented_instruction(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_unimplemented_instruction(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_sparc_unhandled_dcti(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_unhandled_dcti(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_sparc_window_underflow(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_window_underflow(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_a(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_a(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_n(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_n(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_ne(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_ne(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_e(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_e(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_g(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_g(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_le(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_le(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_ge(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_ge(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_l(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_l(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_gu(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_gu(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_leu(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_leu(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_cc(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_cc(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_cs(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_cs(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_pos(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_pos(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_neg(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_neg(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_vc(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_vc(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_sparc_trap_cond_vs(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc_trap_cond_vs(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_sparc32_emulate_instruction(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc32_emulate_instruction(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_sparc64_emulate_instruction(Memory *);
+[[gnu::used]] extern Memory *__remill_sparc64_emulate_instruction(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *
-__remill_ppc_emulate_instruction(Memory *);
+[[gnu::used]] extern Memory *__remill_ppc_emulate_instruction(Memory *);
 
-[[gnu::used, gnu::const]] extern Memory *__remill_ppc_syscall(Memory *);
+[[gnu::used]] extern Memory *__remill_ppc_syscall(Memory *);
 
 }  // extern C
